342a26d84509ae92ade558ac5a1316298169daf4
Jordan Fear <jordanfear1989@gmail.com>
2019-04-11 14:46:59 +0000

    added login

--- app.js
+++ app.js
@@ -10,18 +10,26 @@ var express     = require("express"),
 
 
 
-// mongoose.connect("mongodb+srv://jordanlfc1989:Salah3848!@cluster0-lzsn2.mongodb.net/yelp_camp?retryWrites=true");
+ mongoose.connect("mongodb+srv://jordanlfc1989:Salah3848!@cluster0-lzsn2.mongodb.net/recsite?retryWrites=true");
 // mongoose.connect('mongodb://localhost:27017/yelp_camp_v11', {useNewUrlParser: true});
 // mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
 
 
+
+//--setup
+
 app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
+//seedDB(); //seed the database
 
-
-app.set("view engine", "ejs");
-
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "omg hello !",
+    resave: false,
+    saveUninitialized: false
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
@@ -30,18 +38,57 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use(function(req, res, next){
     res.locals.currentUser = req.user;
-    next();
+   next();
 });
 
+//-- setup--end 
 
+
+
+//-- functions 
+
+const authenticator = (passport.authenticate('local', {
+        successRedirect:'/staff_portal',
+        failureRedirect:'/login'
+}));
+
+
+//-- functions--end 
+
+
+
+
+//--get routes
 
 app.get('/', (req,res) => res.render('index'))
 app.get('/about', (req,res) => res.render('about'))
 app.get('/login', (req,res) => res.render('login'))
 app.get('/jobs', (req,res) => res.render('jobs'))
+app.get('/1234/create/new/newadmin/create1234', (req,res) => res.render('reg'))
+app.get('/staff_portal', (req,res)=> res.render('portal'))
+app.get('/logout', (req,res) => (req.logout() ,res.redirect('/jobs')));
 
+//--get routes--end 
 
-app.post('/login',(req,res) => res.send('login'))
+//--post routes
+app.post('/login', authenticator ,(req,res,err) => {
+    
+});
+
+app.post("/1234/create/new/newadmin/create1234", function(req, res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err)
+            return res.render('reg');
+        }
+        passport.authenticate("local")(req, res, function(){
+           res.redirect('/staff_portal'); 
+        });
+    });
+});
+
+//-post route--end
 
 
 
